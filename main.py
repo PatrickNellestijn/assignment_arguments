52c22a441f079bbf41b276783236a2e91d9becf6
Patrick Nellestijn <83159995+PatrickNellestijn@users.noreply.github.com>
2021-06-14 21:24:17 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,20 @@
 # Do not modify these lines
 __winc_id__ = '7b9401ad7f544be2a23321292dd61cb6'
 __human_name__ = 'arguments'
-
+# Patrick Nellestijn
 # Add your code after this line
-from types import BuiltinMethodType
-
 #Part 1: Greet Template
 def greet(name, greeting_template= 'Hello, <name>!'):
     greeting_template = greeting_template.replace('<name>', 'name')
     return name
 
 #Part 2: Force
-#hard de gravity op een decimaal afronden
+#waarde force op 1 decimaal afronden
 #body heeft de waarde 
 def force(mass, body='earth'):
      planet_gravity = {'sun':274.0, 'jupiter':24.92, 'neptune':11.15, 'saturn':10.44, 'earth':9.798, 'uranus':8.87, 'venus':8.87, 'mars':3.71, 'mercury':3.71, 'moon':1.62, 'pluto':0.58} 
      body_gravity = planet_gravity.get(body) 
-     force = round((mass*body_gravity), 1) #geeft foutmelding mbt afronding
+     force = round((mass*body_gravity), 1) 
      return force
 
 #Part 3: Gravity
