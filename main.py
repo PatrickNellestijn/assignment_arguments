6ddc2553fb3abf438d94c17708f7ab63ce24ddc2
Patrick Nellestijn <83159995+PatrickNellestijn@users.noreply.github.com>
2021-06-14 21:19:34 +0200

    Update main.py

--- main.py
+++ main.py
@@ -15,12 +15,12 @@ def greet(name, greeting_template= 'Hello, <name>!'):
 #body heeft de waarde 
 def force(mass, body='earth'):
      planet_gravity = {'sun':274.0, 'jupiter':24.92, 'neptune':11.15, 'saturn':10.44, 'earth':9.798, 'uranus':8.87, 'venus':8.87, 'mars':3.71, 'mercury':3.71, 'moon':1.62, 'pluto':0.58} 
-     body_gravity=planet_gravity.get(body) 
-     force=round((mass*body_gravity), 1) #geeft foutmelding mbt afronding
+     body_gravity = planet_gravity.get(body) 
+     force = round((mass*body_gravity), 1) #geeft foutmelding mbt afronding
      return force
 
 #Part 3: Gravity
 #formule kun je van de site overnemen
 def pull(m1, m2, d):
-    G=6.674*((m1*m2)/(d**2))
+    G = 6.674 *((m1*m2)/(d**2))
     return pull
