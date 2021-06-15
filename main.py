d5538b54159fb4b20113cb7c42146d7e35bc2c84
Patrick Nellestijn <83159995+PatrickNellestijn@users.noreply.github.com>
2021-06-15 13:52:37 +0200

    Update main.py
    Versie na opmerkingen docent, versie 2

--- main.py
+++ main.py
@@ -5,20 +5,19 @@ __human_name__ = 'arguments'
 # Add your code after this line
 #Part 1: Greet Template
 def greet(name, greeting_template= 'Hello, <name>!'):
-    greeting_template = greeting_template.replace('<name>', 'name')
-    return name
-
+    greeting_template = greeting_template.replace('<name>', name)
+    return greeting_template
 #Part 2: Force
-#waarde force op 1 decimaal afronden
+#hard de gravity op een decimaal afronden
 #body heeft de waarde 
 def force(mass, body='earth'):
-     planet_gravity = {'sun':274.0, 'jupiter':24.92, 'neptune':11.15, 'saturn':10.44, 'earth':9.798, 'uranus':8.87, 'venus':8.87, 'mars':3.71, 'mercury':3.71, 'moon':1.62, 'pluto':0.58} 
-     body_gravity = planet_gravity.get(body) 
-     force = round((mass*body_gravity), 1) 
-     return force
-
+     planet_gravity = {'sun':274, 'jupiter':24.9, 'neptune':11.2, 'saturn':10.4, 'earth':9.8, 'uranus':8.9, 'venus':8.9, 'mars':3.7, 'mercury':3.7, 'moon':1.6, 'pluto':0.6} 
+     body_gravity=planet_gravity.get(body) 
+     calculation_output = mass*body_gravity
+     return calculation_output
 #Part 3: Gravity
 #formule kun je van de site overnemen
 def pull(m1, m2, d):
-    G = 6.674 *((m1*m2)/(d**2))
-    return pull
+    G=6.674 *(10**-11)
+    pull = G *((m1*m2)/(d**2)) 
+    return pull 
